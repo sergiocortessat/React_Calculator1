40e3fa7809662ca622f0b2878a7f662686b77b28
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-05 13:46:35 +0200

    Add calculate logic

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ buttonName }) => (
-  <button type="button">{buttonName}</button>
+const Button = ({ buttonName, handleClick }) => (
+  <button
+    type="button"
+    value={buttonName}
+    onClick={() => handleClick(buttonName)}
+  >
+    {buttonName}
+  </button>
 );
 
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
+  handleClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  handleClick: undefined,
 };
 
 export default Button;
