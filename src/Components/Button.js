a800360e8864df01fd2d5603bff20fcd5964bd8c
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-05 10:29:21 +0200

    Add burton.js

---
+++ src/Components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ buttonName }) => (
+  <button type="button">{buttonName}</button>
+);
+
+Button.propTypes = {
+  buttonName: PropTypes.string.isRequired,
+};
+
+export default Button;
