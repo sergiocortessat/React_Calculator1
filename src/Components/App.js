cc4c2b3bf93dee08126eb7bd12c9a4ad53d38c3b
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-05 13:45:32 +0200

    Add app.js logic

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,39 @@
-import React, { Fragment } from 'react';
+/* eslint-disable react/no-unused-state */
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../Logic/calculate';
 
-function App() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { total, next, operation } = this.state;
+    const data = { total, next, operation };
+    const result = Calculate(data, buttonName);
+    console.log(result)
+    this.setState({
+      total: result.total,
+      next: result.next,
+      operation: result.operation,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Welcome to Math Magicians</h4>
+        <Display />
+        <ButtonPanel handleClick={this.handleClick} />
+      </div>
+    );
+  }
 }
-
-export default App;
