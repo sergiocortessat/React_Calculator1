67a4c40a12d8a988ad5974cdd658e8667a43f1b4
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-06 07:53:11 +0200

    Add pwa network first startegy

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,7 +11,8 @@ import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import {NetworkFirst} from 'workbox-strategies';
+
 
 clientsClaim();
 
@@ -50,13 +51,14 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
+  ({url}) => url.pathname.startsWith('/social-timeline/'),
   ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
+  new NetworkFirst({
     cacheName: 'images',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
+      new ExpirationPlugin({ maxEntries: 10 }),
     ],
   })
 );
