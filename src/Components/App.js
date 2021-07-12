eabe42c1946481a380c33baa750437705619e833
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-12 13:24:53 +0200

    Add refactor calculator to hooks

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,46 +1,38 @@
-/* eslint-disable react/no-unused-state */
-import React, { Component, Fragment } from 'react';
+import { useState, useEffect } from 'react';
+import Calculate from '../Logic/calculate';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import Calculate from '../Logic/calculate';
 import logic from '../Logic/logic';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(buttonName) {
-    const { total, next, operation } = this.state;
-    const data = { total, next, operation };
+const App = () => {
+  const [total, setTotal] = useState(null);
+  const [operation, setOperation] = useState(null);
+  const [next, setNext] = useState(null);
+  const [result, setResult] = useState(null);
+  const handleClick = (buttonName) => {
+    const data = { total, operation, next };
     const result = Calculate(data, buttonName);
-    this.setState({
-      total: result.total,
-      next: result.next,
-      operation: result.operation,
-    });
-  }
+    setTotal(result.total);
+    setOperation(result.operation);
+    setNext(result.next);
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    const result = logic({ total, next, operation });
-    return (
-      <>
-        <h2 className="title">Welcome to math magicians</h2>
-        <div className="app">
-          <h4 className="title-2 text-center">The Matrix Calculator</h4>
-          <div>
-            <Display result={result} />
-            <ButtonPanel clickHandler={this.handleClick} />
-          </div>
+  useEffect(() => {
+    const resultLogic = logic({ total, next, operation });
+    setResult(resultLogic);
+  }, [result, total, next, operation]);
+
+  return (
+    <>
+      <h2 className="title">Welcome to math magicians</h2>
+      <div className="app">
+        <h4 className="title-2 text-center">The Matrix Calculator</h4>
+        <div>
+          <Display result={result} />
+          <ButtonPanel clickHandler={handleClick} />
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
+export default App;
